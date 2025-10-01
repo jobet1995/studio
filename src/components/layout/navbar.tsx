c377778f14cecb15e7a4191648f6e63e70eecbb4
Jobet Casquejo <jobetcasquejo99@gmail.com>
2025-10-01 03:53:16 +0000

    'PawPrint' is declared but its value is never read.

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { Menu, PawPrint } from 'lucide-react';
+import { Menu } from 'lucide-react';
 import React from 'react';
 
 import { cn } from '@/lib/utils';
